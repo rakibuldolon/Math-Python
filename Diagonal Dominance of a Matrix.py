5949bb07fbd8478afc4d842f7438e200947792c5
Rakibul Alam Dolon <55651133+rakibuldolon@users.noreply.github.com>
2023-03-11 00:45:02 +0600

    Create Diagonal Dominance of a Matrix.py

---
+++ Diagonal Dominance of a Matrix.py
@@ -0,0 +1,14 @@
+import numpy as np
+size=int(input('Enter the number of entries in the matrix:')) #size=(number of rows)*(number of columns)
+arr=np.array([],dtype=int)
+print('Enter the entries of the matrix and press enter.') # Enter matrix from 1st element
+for i in range(size):
+    entry=int(input())
+    arr=np.append(arr,entry)
+matrix=arr.reshape(int(np.sqrt(size)),int(np.sqrt(size)))
+D = np.diag(np.abs(matrix))
+S = np.sum(np.abs(matrix), axis=1) - D
+if np.all(D >= S):
+        print('The matrix is diagonally dominant')
+else:
+        print('The matrix is not diagonally dominant')
